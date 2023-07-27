af7b7f57a97c6c94156d677663487a4b2b91e889
Shawn <shawnbrand.com@gmail.com>
2023-07-27 11:07:50 +0800

    I am on C now

--- Student.py
+++
@@ -1,7 +0,0 @@
-class Student:
-
-    def __init__(self,name,major,gpa,is_male):
-        self.name = name
-        self.major = major
-        self.gpa = gpa
-        self.is_mail = is_male
---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("Input your age:");
+    scanf("%d", &age);
+    printf("your age is %d", age);
+    return 0;
+}
